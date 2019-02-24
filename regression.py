a3ee46f8d5162b0ce3b55e2052d94a500a52b00d
Arsen Avagyan <avagyanarpi5@gmail.com>
2019-02-24 22:06:06 +0400

    regression.py

---
+++ regression.py
@@ -0,0 +1,14 @@
+class  Organizm:
+    '''This  class  describe  organizm'''
+    def __init__(self,theheartbeat,theplastic,theage):
+        self.heartbeat=theheartbeat
+        self.plastic=theplastic
+        self.age=theage
+organizm1=Organizm(100,180,20)
+organizm2=Organizm(80,90,30)
+def stugel(organizm):
+    a= (organizm.heartbeat+organizm.plastic+organizm.age)/3
+    if a>=10:
+        print('organizm  is  healthly')
+stugel(organizm1)
+stugel(organizm2)
