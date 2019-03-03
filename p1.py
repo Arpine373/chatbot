21ad2b2aa5af9f35352e5509c336896030d76421
Arsen Avagyan <avagyanarpi5@gmail.com>
2019-03-03 21:20:02 +0400

    p1.py

---
+++ p1.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_boston
+data = load_boston()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target)
+from sklearn.linear_model import LinearRegression
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+expected = y_test
