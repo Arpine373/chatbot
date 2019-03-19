8e4d09dae3327e9440490ee4ea3b21163d5a042f
Arsen Avagyan <avagyanarpi5@gmail.com>
2019-03-19 22:14:30 +0400

    lesson5

--- lesson 5/norvarj3.py
+++ lesson 5/norvarj3.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 dzaynner = {'anunov':['soprano', 'bass', 'alt'],
 'tverov':[15, 16, 17]}
 a = pd.DataFrame(dzaynner)
 print(a)
 b= pd.DataFrame(dzaynner, index=['one','two','three'])
-print(a.plot.bar())
-print(b.plot())
+a.plot.bar()
+b.plot()
+plt.show()
